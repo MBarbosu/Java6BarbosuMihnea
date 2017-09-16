dab92e3c74e374c4ccd336060c94ecebf627ddda
SkullFlight <SkullFlight@DESKTOP-GN2A565>
2017-09-16 17:05:38 +0300

    This is my first program in Java

---
+++ java6BarbosuMihnea/src/firstprogram/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * My first program written in Java
+ */
+package firstprogram;
+
+/**
+ *
+ * This is my first Class
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+    
+}
